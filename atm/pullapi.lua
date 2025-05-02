4734973981268fe8667463ce990fa35310d3b505
A2noh <juan.bohacek@gmail.com>
2025-05-02 17:39:21 +0200

    testing enderchest

--- atm/pullapi.lua
+++ atm/pullapi.lua
@@ -1,26 +1,29 @@
---ATM version 0.0.7--
---Must be called pullapi.lua--
+--ATM Pull API 0.1 with Turtle + Ender Chest Delivery
 
-local chests = {"minecraft:chest_0"}
-
+local chests = {"enderstorage:ender_chest_0"}
 local dropper = "minecraft:dropper_0"
 local junk = "minecraft:dropper_1"
-
 local coin = "minecraft:diamond"
 
 local d = peripheral.wrap(dropper)
 local j = peripheral.wrap(junk)
 
+-- Map of ATM IDs to color codes or delivery channels
+local atmDeliveryMap = {
+    [1] = "white_white_white", -- Example: ATM ID 1
+    [2] = "red_white_blue",
+    -- Add more as needed
+}
 
 function getStored()
     local count = 0
-    for k, v in pairs(chests) do
-        cs = peripheral.wrap(v)
-        for k2, v2 in pairs(cs.list()) do
-            if v2.name == coin then
-                count = count + v2.count
+    for _, chestName in pairs(chests) do
+        local chest = peripheral.wrap(chestName)
+        for slot, item in pairs(chest.list()) do
+            if item.name == coin then
+                count = count + item.count
             else
-                j.pullItems(v, k2, v2.count)
+                j.pullItems(chestName, slot, item.count)
             end
         end
     end
@@ -29,13 +32,13 @@ end
 
 function deposit()
     local count = 0
-    for k, v in pairs(chests) do
+    for _, chestName in pairs(chests) do
         for i = 1, 9 do
-            local tab = d.getItemDetail(i)
-            if tab ~= nil then
-                if tab.name == coin then
-                    local deped = d.pushItems(v, i, 64)
-                    count = count + deped
+            local item = d.getItemDetail(i)
+            if item then
+                if item.name == coin then
+                    local deposited = d.pushItems(chestName, i, 64)
+                    count = count + deposited
                 else
                     d.pushItems(junk, i, 64)
                 end
@@ -45,39 +48,51 @@ function deposit()
     if count > 0 then
         return true, count
     else
-        return false, "Diamonds enterd must be more than 0"
+        return false, "Deposited amount must be more than 0"
     end
 end
 
-function withdraw(amount)
+function withdraw(amount, atmID)
     if amount > 576 then
-        return false, "amount must be at maximum 9 stacks or 576 items"
+        return false, "Max withdrawal is 576"
     end
     if amount > getStored() then
-        return false, "not enough money in storage"
+        return false, "Insufficient bank balance"
     end
-    
-    local count = 0
-    for k, v in pairs(chests) do
-        cs = peripheral.wrap(v)
+
+    -- Pull coins into turtle's inventory
+    local turtleName = "turtle_0"
+    local delivered = 0
+
+    for _, chestName in pairs(chests) do
+        local chest = peripheral.wrap(chestName)
         for i = 1, 54 do
-            tab = cs.getItemDetail(i)
-            if tab ~= nil then
-                if tab.name == coin then
-                    if (count + tab.count) > amount then
-                        d.pullItems(v, i, (amount - count))
-                        return true
-                    else
-                        d.pullItems(v, i, tab.count)
-                        count = count + tab.count
-                    end
-                    if count == amount then
-                        return true
-                    end
-                else
-                    j.pullItems(chests, i, 64)
-                end
+            if delivered >= amount then break end
+            local item = chest.getItemDetail(i)
+            if item and item.name == coin then
+                local toMove = math.min(item.count, amount - delivered)
+                peripheral.call(turtleName, "pullItems", chestName, i, toMove)
+                delivered = delivered + toMove
             end
         end
     end
+
+    if delivered < amount then
+        return false, "Could not gather enough coins"
+    end
+
+    -- Deliver via ender chest
+    if not atmDeliveryMap[atmID] then
+        return false, "Unknown ATM ID"
+    end
+
+    local chestColor = atmDeliveryMap[atmID]
+
+    rednet.send(peripheral.getName(peripheral.wrap(turtleName)), textutils.serialize({
+        command = "deliver",
+        toColor = chestColor,
+        count = amount
+    }))
+
+    return true, "Delivery started"
 end
--- turtle.lua
+++ turtle.lua
@@ -1,1 +1,23 @@
-turtle.suck()
+rednet.open("back")
+print("Turtle ready for delivery.")
+
+while true do
+    local _, msg = rednet.receive()
+    local data = textutils.unserialize(msg)
+    if data and data.command == "deliver" then
+        local color = data.toColor
+        local count = data.count or 0
+
+        -- Optional: open Ender Chest with matching color (assumed already placed)
+        print("Delivering " .. count .. " coins to " .. color)
+
+        for i = 1, 16 do
+            local item = turtle.getItemDetail(i)
+            if item and item.name == "minecraft:diamond" then
+                turtle.select(i)
+                turtle.drop() -- Assumes turtle is facing into Ender Chest
+            end
+        end
+        rednet.send(_, "Delivered to ATM: " .. color)
+    end
+end
