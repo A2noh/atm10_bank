277af3b239c1e7f6771cce34e8a2961f9d9d9ef9
A2noh <juan.bohacek@gmail.com>
2025-05-03 11:07:41 +0200

    dunno

--- turtle.lua
+++ turtle.lua
@@ -1,9 +1,9 @@
-rednet.open("back")
+rednet.open("right")
 print("Turtle ready for delivery.")
 
 while true do
-    local _, msg = rednet.receive()
-    local data = textutils.unserialize(msg)
+    local senderID, msg = rednet.receive()
+    local data = msg
     if data and data.command == "deliver" then
         local color = data.toColor
         local count = data.count or 0
@@ -18,6 +18,6 @@ while true do
                 turtle.drop() -- Assumes turtle is facing into Ender Chest
             end
         end
-        rednet.send(_, "Delivered to ATM: " .. color)
+        rednet.send(senderID, "Delivered to ATM: " .. color)
     end
 end
