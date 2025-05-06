2ef1aba467597b9e56d0f8c6b6b6f61e469845b7
A2noh <juan.bohacek@gmail.com>
2025-05-06 12:11:44 +0200

    edit api.lua for tutel

--- atm/api.lua
+++ atm/api.lua
@@ -53,8 +53,27 @@ function withdraw(account, amount, ATM, pin)
     rednet.send(server, msg, "banking")
     local send, mes, pro = rednet.receive("banking")
     if mes[1] == "witR" then
-        return mes[2], mes[3], amount
+        -- Bank server confirmed withdrawal is valid
+        local success, response = true, mes[2]
+
+        -- 🐢 Send request to turtle for physical delivery
+        local TURTLE_ID = 4  -- ✅ change this to your actual turtle ID
+        local delivery = {
+            command = "deliver",
+            count = amount
+        }
+
+        print("Sending delivery request to Turtle ID " .. TURTLE_ID)
+        rednet.send(TURTLE_ID, delivery)
+
+        local _, turtleReply = rednet.receive(3)  -- wait up to 3 seconds
+        if turtleReply then
+            return success, response .. " | " .. turtleReply
+        else
+            return success, response .. " | Delivery request sent, no turtle reply"
+        end
     end
+
     return false, "oof"
 end
 
